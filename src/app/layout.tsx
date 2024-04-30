55c058a34599a6f0aa6087b6ca63464c2c0953e9
esHome33 <etienschwa@gmail.com>
2024-04-30 09:55:31 +0200

    icons and metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Revolve simulator - casse tête en bois",
   description: "Revolve - Tour de billes - wooden game simulator - cylinder brain teaser - simulateur de casse tête cylindrique",
+  generator: "Next.js",
+  keywords: ["nextjs", "puzzle game", "Revolve", "simulator", "casse tête", "simulateur",
+    "wooden game", "jeu en bois", "Rubiks cube",
+    "Tour de billes", "cylinder brain teaser", "brain teaser"],
+  authors: {
+    name: "ESHome33",
+    url: "https://github.com/esHome33"
+  },
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+
 };
 
 export default function RootLayout({
